e504192091eb46c4fc1fd1cbf50ee135e61c8af4
Yosua Kristianto Linux <yosua_kristianto144@outrlok.com>
2025-08-18 13:05:22 +0700

    Add commentaries

--- src/controller/text_analyzer/controller_handler.py
+++ src/controller/text_analyzer/controller_handler.py
@@ -1,12 +1,18 @@
 
 
 def handle_text_analysis(text: str) -> str:
-    """This handler perform text analysis by taking the input text,
-    and returning a simple analysis result as image's src that is related to the text based-on sentiment.
+    """This handler function processes as bridge input text to perform sentiment analysis, 
+    and take control of the logic to return the CDN URL of an image related to the sentiment of the text.
 
-    
+    The sentiment analysis logic is applied in the ML dedicated service layer.
+
+    Args:
+        text (str): The input text to analyze.
+
+    Returns:
+        str: The CDN URL of the image related to the sentiment of the text.
     """
 
-    
+        
 
-    return ""d
+    return ""
