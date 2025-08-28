0e5ebc69518426cfe97eeb96daef7d1c54157a18
Yosua Kristianto (Legion) <yosua_kristianto144@outlook.com>
2025-08-28 15:29:23 +0700

    Added debugger

---
+++ src/controller/debugger/controller.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, Request
+from config.config import settings
+
+from model.object.base_response_dto import BaseResponse
+
+from common.facade.encryption import hash
+
+import json
+
+class DebuggerController:
+
+    router = APIRouter()
+
+    @router.post("/hash")
+    async def get_token_hasher(request: Request):
+        """DEBUG ONLY
+
+        Generate token based-on request.
+        
+        -- """
+
+        if(not settings.app_debug):
+            raise Exception("This endpoint is only available in debug mode.")
+
+        def minify_json(json_str: str) -> str:
+            parsed = json.loads(json_str)  # parse into Python dict
+            return json.dumps(parsed, separators=(",", ":"))  # minify (no spaces/newlines)
+
+        body = await request.body()
+
+        json_request = minify_json(body.decode("utf-8"))
+
+        return BaseResponse.ok(
+            message = "Hash generated",
+            data = {
+                "token": hash(json_request),
+                "original_request": json_request
+            }
+        )
+
+
+    
