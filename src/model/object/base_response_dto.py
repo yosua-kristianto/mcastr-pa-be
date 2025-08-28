ba63e929d2f4e6dce22bf19547ae78ec56e4a0a3
Yosua Kristianto (Legion) <yosua_kristianto144@outlook.com>
2025-08-28 21:32:32 +0700

    Added BaseResponse Exception

--- src/model/object/base_response_dto.py
+++ src/model/object/base_response_dto.py
@@ -1,16 +1,32 @@
 
 class BaseResponse:
 
-    def __init__(self, message: str, code: int, status: bool, data = None):
-        self.message = message;
-        self.code = code;
-        self.status = status;
-        self.data = data;
+    def __init__(self, message: str, code: str, status: bool, data = None):
+        self.message = message
+        self.code = code
+        self.status = status
+        self.data = data
     
     @staticmethod
     def ok(message="Ok", data=None):
-        return BaseResponse(message, code="200", status = True, data = data);
+        return BaseResponse(message, code="200", status = True, data = data)
 
-    @staticmethod
-    def error(message="Internal Server Error", code="500"):
-        return BaseResponse(message, code, False, None);
+class BaseResponseException(Exception):
+    """I don't know why FastAPI requires this type of setup.
+    But let's just follow the lead.
+
+    """
+
+    def __init__(self, message: str, status_code: int = 400, error_code: str = ""):
+        self.name = message
+        self.status_code = status_code
+        self.code = error_code
+
+    def error_response(self):
+        # return BaseResponse(self.name, self.code, False, None)
+        return {
+            "status": False,
+            "code": self.code,
+            "message": self.name,
+            "data": None
+        }
