b675d6a051ea655f3fcbc9766fb1b60c4a734a78
Yosua Kristianto (Legion) <yosua_kristianto144@outlook.com>
2025-08-28 15:35:06 +0700

    Revise feedback_uri as it may be misleading

--- src/model/object/response/TextAnalysisResponseDTO.py
+++ src/model/object/response/TextAnalysisResponseDTO.py
@@ -3,5 +3,5 @@ class TextAnalysisResponseDTO:
 
     def __init__(self, image_uri: str = "", feedback_uri: str = ""):
         self.image_uri = image_uri
-        self.feedback_uri = feedback_uri
+        self.feedback_id = feedback_uri
     
