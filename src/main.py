1309f543922c22db009b41a0d927ae17de6760ed
Yosua Kristianto (Legion) <yosua_kristianto144@outlook.com>
2025-11-14 22:18:18 +0700

    Fixing cors problem from local to local

--- src/main.py
+++ src/main.py
@@ -4,12 +4,23 @@ from routes.routes import route
 
 app = FastAPI()
 
-
-
-
-
 from middleware import Authenticate
+from fastapi.middleware.cors import CORSMiddleware
+
 app.add_middleware(Authenticate)
+
+origins = [
+    "http://127.0.0.1:5173",
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(route)
 
